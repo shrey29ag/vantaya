e38d77684a2eb1d47de4d07e1c3d518de2ec9872
shrey29ag <shreyvinayakagrawal1111@gmail.com>
2026-02-15 04:12:33 +0530

    Redirect root route to listings

--- app.js
+++ app.js
@@ -62,9 +62,9 @@ const sessionOptions = {
     }
 };
 
-// app.get("/", (req, res) => {
-//     res.send("Working...");
-// });
+app.get("/", (req, res) => {
+    res.redirect("/listings");
+});
 
 app.use(session(sessionOptions));
 app.use(flash());
